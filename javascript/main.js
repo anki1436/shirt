4b203c2f4f00d2e37e60b4190363f59daaa14d88
anki1436 <ankitbisht.neosoft@gmail.com>
2022-04-13 11:48:05 +0530

    shirt palace

---
+++ javascript/main.js
@@ -0,0 +1,40 @@
+let menuBtn = document.getElementById("menu");
+let closeBtn = document.getElementById("close");
+let navMenu = document.getElementById("navMenu");
+
+
+
+
+
+function menuOpen() {
+console.log(navMenu);
+  navMenu.style.right = "0";
+  console.log(navMenu);
+  // navMenu.style.display = "flex";
+
+
+}
+function menuClose() {
+  console.log(navMenu);
+    navMenu.style.right = "-100%";
+    console.log(navMenu);
+  // navMenu.style.display = "none";
+
+
+  
+  
+  }
+
+
+
+  // $(document).ready(function(){
+
+  //     $('.owl-carousel').owlCarousel({
+  //       center: true,
+  //       items:2,
+  //       loop:true,
+  //       margin:100,
+       
+  //   });
+  // });
+
